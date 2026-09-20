308c16ca8ab17bd3ac92dd74243c96073a362309
BeginL <lilijancika20@gmail.com>
2026-09-20 13:47:49 +0300

    Initial commit

---
+++ src/main/java/lt/vcd/Main.java
@@ -0,0 +1,26 @@
+package lt.vcd;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        Scanner in=new Scanner(System.in);
+
+
+
+
+        System.out.println("Hello ");
+        System.out.println("please enter x value: ");
+        int x=in.nextInt();
+
+        System.out.println( x / y );
+        float y=2;
+
+        System.out.println( x / y );
+
+
+
+     }
+}
